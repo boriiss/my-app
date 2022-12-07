471f1becac6701c2e1b3b34425ec30ca8e13165f
Boris <boriswork1996@gmail.com>
2022-12-07 23:50:20 +0300

    12 урок

---
+++ src/сomponents/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <div className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQwjjXEEAabj31k9UdorR7XaxweOnen8imwA&usqp=CAU" alt="" />
+      </div>
+    );
+}
+
+export default Header;
---
+++ src/сomponents/Profile.jsx
@@ -0,0 +1,30 @@
+const Profile = () => {
+    return (
+      <div className="content">
+        <div>
+          <img src="https://oir.mobi/uploads/posts/2021-06/1624404216_1-oir_mobi-p-bereg-okeana-s-palmami-priroda-krasivo-fot-1.jpg" alt="" />
+        </div>
+        <div className="desription">
+          ava + desription
+        </div>
+        <div className="posts">
+          <h2>
+            My posts
+          </h2>
+          <div className="newPost">
+            New post
+          </div>  
+          <div>
+            <div>
+              post1
+            </div>
+            <div>
+              post2
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
