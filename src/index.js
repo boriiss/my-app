730ada3381090cdde7c9746b8e97504631007867
Boris <boriswork1996@gmail.com>
2022-12-25 02:17:05 +0300

    33 урок

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {rerenderEntireTree} from './render';
 import state from './redux/state';
-import {addPost} from './redux/state';
-import {BrowserRouter} from "react-router-dom";
 
-let rerenderEntireTree = () => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <BrowserRouter>
-      <App state={state} addPost={addPost}/>
-    </BrowserRouter>
-  );
-}
-
-rerenderEntireTree();
+rerenderEntireTree(state);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
