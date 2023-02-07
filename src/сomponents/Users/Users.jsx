3f78bdc979834a988c79dd448963b4d08fdf73fd
Boris <boriswork1996@gmail.com>
2023-02-07 03:23:12 +0300

    56,57 уроки

--- src/сomponents/Users/Users.jsx
+++ src/сomponents/Users/Users.jsx
@@ -4,44 +4,48 @@ import axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
 const Users = (props) => {
-  let getUsers = () => {
-    if(props.users.length === 0) {
-    
-      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        props.setUsers(response.data.items);
-      });
-    }
+  
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+  let pages = [];
+  for(let i=1;i <= pagesCount; i++){
+    pages.push(i);
   }
 
   return (
     <div>
-      <button onClick={getUsers}>Get Users</button>
-        {
-          props.users.map( u => <div key={u.id}>
-            <span>
-              <div>
-                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" className={styles.userPhoto} />
-              </div>
-              <div>
-                { u.followed 
-                    ? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button> 
-                    : <button onClick={() => { props.follow(u.id) }}>Follow</button> }
-              </div>
-            </span>
-            <span>
+      <div>
+        {pages.map( p => {
+          return <span className={props.currentPage === p && styles.selectedPage} 
+          onClick={(e) => {props.onPageChanged(p)}} >{p}</span>
+        })}
+      </div>
+          {
+            props.users.map( u => <div key={u.id}>
               <span>
-                <div>{u.name}</div>
-                <div>{u.status}</div>
+                <div>
+                  <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" className={styles.userPhoto} />
+                </div>
+                <div>
+                  { u.followed 
+                      ? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button> 
+                      : <button onClick={() => { props.follow(u.id) }}>Follow</button> }
+                </div>
               </span>
               <span>
-                <div>{"u.location.country"}</div>
-                <div>{"u.location.city"}</div>
+                <span>
+                  <div>{u.name}</div>
+                  <div>{u.status}</div>
+                </span>
+                <span>
+                  <div>{"u.location.country"}</div>
+                  <div>{"u.location.city"}</div>
+                </span>
               </span>
-            </span>
-          </div>
-          )
-        }
-    </div>
+            </div>
+            )
+          }
+      </div>
   );
 }
 
---
+++ src/сomponents/common/Preloader/Preloader.js
@@ -0,0 +1,5 @@
+let Preloader = (props) => {
+    return <img style={{width: '250px'}} src="https://i.pinimg.com/originals/c8/a1/76/c8a1765ffc8243f3a7b176c0ca84c3c1.gif" />
+}
+
+export default Preloader;
